43c996fa97d2bc60c4c35b331a57734efb563ba5
Lilian <lviennet@gaming.tech>
2022-12-12 17:43:54 +0100

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,35 @@
 import Home from "./pages/Home";
+import Pokedex from "./pages/Pokedex";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
+import "./App.css"
+
 
 //App.js
 function App(props){
-  return <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home /> 
-        </Route>
-      </Switch>
-  </Router>
-}
+  return (
+    <Router>
+      <div className="center">
+        <ul>
+          <Link to="/"><button className="buttonHome left" >Pokemon </button></Link>
+          <Link to="/Pokedex"><button className="buttonHome right" >Pokedex </button></Link>
+        </ul> 
+        <Switch>
+          <Route exact path="/">
+            <Home /> 
+          </Route>
+          <Route path="/Pokedex">
+            <Pokedex />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 
 export default App;
